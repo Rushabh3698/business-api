6ea8279a3c0c9e65cd8a803b2a0fa4268a99cc42
Rushabh3698 <75696688+Rushabh3698@users.noreply.github.com>
2022-03-23 21:17:34 -0700

    Add skelton methods

--- routes/businessOrganization.js
+++ routes/businessOrganization.js
@@ -2,6 +2,24 @@ const express = require('express')
 const router = express.Router()
 //const organiztion = require('../models/organiztion')
 
+//Getting all
+router.get('/',(req,res)=>{
 
-
+})
+//Getting one
+router.get('/:id',(req,res)=>{
+    
+})
+//Adding one
+router.post('/',(req,res)=>{
+    
+})
+//Updating one
+router.patch('/',(req,res)=>{
+    
+})
+//Deleting one
+router.delete('/:id',(req,res)=>{
+    
+})
 module.exports = router
