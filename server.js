93f25e75ce05b34730df39f501ff3e3700c75b43
Rushabh3698 <75696688+Rushabh3698@users.noreply.github.com>
2022-03-24 15:38:12 -0700

    connect to atlas instead of local mongodb

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true}).then(()=>{
+    console.log("connectec to atlas")
+})
 const db = mongoose.connection
 db.on('error',(error)=>console.error(error))
 db.once('open',()=>console.log("Connected to Database"))
